be99d528596f5e196a91f03051c8fc7005991f2a
George <152157371+George15526@users.noreply.github.com>
2024-11-20 14:43:29 +0800

    feat: add a index page for register or login choices

--- app.py
+++ app.py
@@ -2,5 +2,15 @@ from src import create_app
 
 app = create_app()
 
+@app.route('/')
+def index():
+    return (
+    """
+    <h1>Hello, this is index page<h1>
+    <h3>Register: Click <a href="http://127.0.0.1:5000/user/register">here</a><h3>
+    <h3>Login: Click <a href="http://127.0.0.1:5000/user/login">here</a><h3>
+    """
+    )
+
 if __name__ == '__main__':
     app.run(debug=True)
